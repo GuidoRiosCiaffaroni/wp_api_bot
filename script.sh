aae9180a033273d1b98d117d713c8693a5d82f54
guidoriosciaffaroni <guidoriosciaffaroni@gmail.com>
2022-01-08 18:15:18 -0300

    2022_01_08_18_15_18

--- script.sh
+++ script.sh
@@ -42,4 +42,15 @@ done
 URLGet=$URLBase$URLData
 #echo $URLGet
 
-curl $URLGet
+
+
+#$url = "http://www.dominio.com/ejemplo.php?valor=2";
+$url = $URLGet
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+
+
+$str = curl_exec($curl);
+echo $str;
+
